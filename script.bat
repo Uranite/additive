a5567ab787840a69e43ac98d1f6d935ff11666b4
Uranite <62639703+Uranite@users.noreply.github.com>
2025-07-19 17:44:00 +0700

    update

--- script.bat
+++ script.bat
@@ -1,59 +1,66 @@
 @echo off
-setlocal
 
+if "%1" == "" goto update
 if "%1" == "update-packwiz" goto update-packwiz
 if "%1" == "export" goto export
 if "%1" == "update" goto update
 if "%1" == "update-loader" goto update-loader
 if "%1" == "refresh" goto refresh
+if "%1" == "add" goto add
 goto end
 
 :update-packwiz
 	go install github.com/packwiz/packwiz@latest
-	go install github.com/Merith-TK/packwiz-wrapper/cmd/pw@main
 	::cls
 	echo Packwiz has been Updated
 	goto end
 
 :export
 	if not exist build\fabric\ mkdir build\fabric\
-	::for /d %%d in (versions\fabric\*) do (
+	::for /d %%d in (archived\fabric\*) do (
 	::    cd %%d
-	::    pw mr export
+	::    packwiz mr export
 	::    cd ..\..\..
 	::)
-	cd archived\fabric\1.18.2 && pw mr export
+	cd archived\fabric\1.18.2 && packwiz mr export
 	cd ..\..\..
-	::for /R versions\fabric %%f in (*.mrpack) do move "%%f" build\fabric\
+	::for /R archived\fabric %%f in (*.mrpack) do move "%%f" build\fabric\
 	move archived\fabric\1.18.2\*.mrpack build\fabric
 	goto end
 
 :update
-	::for /d %%d in (versions\fabric\*) do (
+	::for /d %%d in (archived\fabric\*) do (
 	::    cd %%d
-	::    pw update --all
+	::    packwiz update --all
 	::    cd ..\..\..
 	::)
-	cd archived\fabric\1.18.2 && pw update --all
+	cd archived\fabric\1.18.2 && packwiz update --all
 	goto end
 
 :update-loader
-	::for /d %%d in (versions\fabric\*) do (
+	::for /d %%d in (archived\fabric\*) do (
 	::    cd %%d
-	::    pw migrate loader latest
+	::    packwiz migrate loader latest
 	::    cd ..\..\..
 	::)
-	cd archived\fabric\1.18.2 && pw migrate loader latest
+	cd archived\fabric\1.18.2 && packwiz migrate loader latest
 	goto end
 
 :refresh
-	::for /d %%d in (versions\fabric\*) do (
+	for /d %%d in (archived\fabric\*) do (
+	    cd %%d
+	    packwiz refresh
+	    cd ..\..\..
+	)
+	goto end
+
+:add
+	::for /d %%d in (archived\fabric\*) do (
 	::    cd %%d
-	::    pw refresh
+	::    packwiz refresh
 	::    cd ..\..\..
 	::)
-	cd archived\fabric\1.18.2 && pw refresh
+	cd archived\fabric\1.18.2 && packwiz mr add "%2"
 	goto end
 
 :end
-endlocal
